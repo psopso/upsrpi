25963fec2492a0b96edb66b08873861a1c365c6a
psopso <57403373+psopso@users.noreply.github.com>
2024-02-29 14:08:39 +0100

    Update sensor.py

--- custom_components/ups_rpi/sensor.py
+++ custom_components/ups_rpi/sensor.py
@@ -19,13 +19,28 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
     """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
+    add_entities([ExampleSensor(),ExampleSensor1()])
 
 
 class ExampleSensor(SensorEntity):
     """Representation of a Sensor."""
 
     _attr_name = "Example Temperature"
+    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._attr_native_value = 23
+
+class ExampleSensor1(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_name = "Example Temperature p"
     _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
     _attr_device_class = SensorDeviceClass.TEMPERATURE
     _attr_state_class = SensorStateClass.MEASUREMENT
