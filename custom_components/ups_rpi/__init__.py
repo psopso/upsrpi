87eff72f85af678153f6f383168b7b7a8187059e
psopso <57403373+psopso@users.noreply.github.com>
2024-02-29 20:31:54 +0100

    Update __init__.py

--- custom_components/ups_rpi/__init__.py
+++ custom_components/ups_rpi/__init__.py
@@ -2,7 +2,7 @@ from .const import DOMAIN
 
 import logging
 
-from homeassitant.core import HomeAssistant
+from homeassistant.core import HomeAssistant
 from homeassistant.const import EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
