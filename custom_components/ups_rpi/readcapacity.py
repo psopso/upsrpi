2ce94cab672d502ed0a61e775fd4c2c24bcd7288
psopso <57403373+psopso@users.noreply.github.com>
2024-02-29 22:28:40 +0100

    Convert EOL

---
+++ custom_components/ups_rpi/readcapacity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import struct
+import sys
+import time
+#import smbus
+from smbus2 import SMBus
+
+
+def readCapacity(bus):
+     address = 0x36
+     read = bus.read_word_data(address, 4)
+     swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+     capacity = swapped/256
+     return capacity
+
+bus = SMBus(1) # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+#print ("%5.2f" % readCapacity(bus))
