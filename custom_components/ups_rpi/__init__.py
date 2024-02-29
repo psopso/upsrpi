379e46d62a8d0efbeaabc58d2299af080cf6298f
psopso <57403373+psopso@users.noreply.github.com>
2024-02-29 07:57:41 +0100

    Update __init__.py

--- custom_components/ups_rpi/__init__.py
+++ custom_components/ups_rpi/__init__.py
@@ -1,5 +1,26 @@
 DOMAIN = "ups_rpi"
+_LOGGER = logging.getLogger(__name__)
 
+import logging
+from homeassistant.const import EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+import threading
+from . import rcswitch
+
+CONF_VAR1 = "var1"
+CONF_VAR2 = "var2"
+
+REQ_LOCK = threading.Lock()
+CONFIG_SCHEMA = vol.Schema(
+	{
+		DOMAIN: vol.Schema({
+			vol.Required(CONF_VAR1): cv.string,
+			vol.Optional(CONF_VAR2, default=9600): cv.positive_int,
+		})
+	},
+	extra=vol.ALLOW_EXTRA,
+)
 
 async def async_setup(hass, config):
 #    hass.states.async_set("hello_state.world", "Paulus")
