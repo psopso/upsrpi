af7ef07e7f3d577334d73838ff96f8038d0dc9a7
psopso <57403373+psopso@users.noreply.github.com>
2024-02-29 22:23:56 +0100

    Update readvoltage.py

--- custom_components/ups_rpi/readvoltage.py
+++ custom_components/ups_rpi/readvoltage.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
-import logging
 import struct
+#import smbus
 import sys
 import time
-
 from smbus2 import SMBus
 
-_LOGGER = logging.getLogger(__name__)
+def readVoltage(bus):
+     address = 0x36
+     read = bus.read_word_data(address, 2)
+     swapped = struct.unpack("<H", struct.pack(">H", read))[0]
+     voltage = swapped * 1.25 /1000/16
+     return voltage
 
-bus = SMBus(1)
 
-_LOGGER.warning("Debug readvoltage")
+bus = SMBus(1) # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+#print ("%5.2f" % readVoltage(bus))
